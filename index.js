8168f3b789b22750aeae274fe69a56c52751ba03
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-11 04:20:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+let recipe = {
+  name: "",
+  description: "",
+  ingredients: [ "","","","",""]
+};
+
 function init() {
   //put any page initialization/handlebars initialization here
   Handlebars.registerHelper('display_ingredient', function() {
-    return new Handlebars.SafeString(this.ingredient)
+    return new Handlebars.SafeString(this)
 })
 }
 document.addEventListener("DOMContentLoaded", function(event) {
@@ -9,12 +15,10 @@ document.addEventListener("DOMContentLoaded", function(event) {
 })
 
 function createRecipeForm() {
-  let recipe = {
-    name: "",
-    description: "",
-  }
   let template = Handlebars.compile(document.getElementById("recipe-form-template").innerHTML);
-  var result = template(recipe);
+  recipe.name = "";
+  recipe.description = "";
+  let result = template(recipe);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
 function createRecipe() {
