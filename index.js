910efed766ff26458bc56ea103c658f60cfc6dcc
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-09-11 05:04:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,56 +6,60 @@ function initForm() {
 
 function createRecipe() {
   var recipe = getRecipe();
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
   var template = Handlebars.compile(recipeTemplate);
   // replace main contents (recipe form) with the recipe template
-  document.getElementById("main").innerHTML = template(recipe)
+  document.getElementById("main").innerHTML = template(recipe);
 }
 
 function updateRecipe() {
   var recipe = getRecipe();
-  var recipeTemplate = document.getElementById("recipe-template").innerHTML
-  var template = Handlebars.compile(recipeTemplate)
-  document.getElementById("main").innerHTML = template(recipe)
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML;
+  var template = Handlebars.compile(recipeTemplate);
+  document.getElementById("main").innerHTML = template(recipe);
 }
 
 function displayEditForm() {
-  var name = document.getElementById("recipeName").innerText
-  var description = document.getElementById("recipeDescription").innerText
-  var ingredientsNodes = document.getElementsByName("ingredientsList")
-  var ingredients = []
+  var name = document.getElementById("recipeName").innerText;
+  var description = document.getElementById("recipeDescription").innerText;
+  var ingredientsNodes = document.getElementsByName("ingredientsList");
+  var ingredients = [];
   for(var i=0;i<ingredientsNodes.length;i++) {
-    ingredients.push(ingredientsNodes[i].innerText)
+    ingredients.push(ingredientsNodes[i].innerText);
   }
 
-  var recipe = {name, description, ingredients, action: 'createRecipe()'}
+  var recipe = {name, description, ingredients, action: 'createRecipe()'};
 
-  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
-  var template = Handlebars.compile(recipeFormTemplate)
-  document.getElementById("main").innerHTML = template(recipe)
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML;
+  var template = Handlebars.compile(recipeFormTemplate);
+  document.getElementById("main").innerHTML = template(recipe);
 }
 
 function getRecipe() {
-  var ingredientsNodes = document.getElementsByName("ingredients")
-  var ingredients = []
+  var ingredientsNodes = document.getElementsByName("ingredients");
+  var ingredients = [];
   for(var i=0;i<ingredientsNodes.length;i++) {
     if(ingredientsNodes[i].value !== "") {
-      ingredients.push(ingredientsNodes[i].value)
+      ingredients.push(ingredientsNodes[i].value);
     }
   }
-  var name = document.getElementById("name").value
-  var description = document.getElementById("description").value
-  var recipe = {name, ingredients, description}
-  return(recipe)
+  var name = document.getElementById("name").value;
+  var description = document.getElementById("description").value;
+  var recipe = {name, ingredients, description};
+  return(recipe);
 }
-// instead of jquery, can use following; document.addEventListener("DOMContentLoaded", function(event) {
-$( document ).ready(function() {
+
+function init() {
   //put any handlebars registrations here.
   Handlebars.registerHelper('displayIngredient', function(ingredient) {
     return new Handlebars.SafeString('<li name="ingredientsList">' + ingredient + '</li>')
-  })
-  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
-  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
+  });
+  Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML);
+  Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML);
 
-  initForm()
+  initForm();
+}
+// instead of jquery, can use following; document.addEventListener("DOMContentLoaded", function(event) {
+$( document ).ready(function() {
+  init();
 });
